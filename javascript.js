79b7ab14a2f5c3bc7a546b29fdae4cd734b02155
Christina <ch.dietrichs@yahoo.no>
2025-02-13 10:37:57 +0100

    Add rules for puzzlePieces

--- javascript.js
+++ javascript.js
@@ -1,20 +1,20 @@
 
 //Model
 
-let verticalPosition = 24; 
+let verticalPosition = 24;
 let horizontalPosition = 0;
 let timesAdded = 0;
 let jigsawColor = 2;
 
-addPuzzlePiece('buttons', true, true, true, true, 'evaluatePiece(true,true,true,true)', 1);
-addPuzzlePiece('buttons', false, true, true, true, 'evaluatePiece(false,true,true,true)', 2);
-addPuzzlePiece('buttons', true, false, true, true, 'evaluatePiece(true,false,true,true)', 1);
-addPuzzlePiece('buttons', true, true, false, true, 'evaluatePiece(true,true,false,true)', 2);
-addPuzzlePiece('buttons', true, true, true, false, 'evaluatePiece(true,true,true,false)', 1);
-addPuzzlePiece('buttons', false, true, true, false, 'evaluatePiece(false,true,true,false)', 2);
-addPuzzlePiece('buttons', false, false, true, true, 'evaluatePiece(false,false,true,true)', 1);
-addPuzzlePiece('buttons', true, true, false, false, 'evaluatePiece(true,true,false,false)', 2);
-addPuzzlePiece('buttons', true, false, false, true, 'evaluatePiece(true,false,false,true)', 1);
+addPuzzlePiece('buttons', true, true, true, true, 'evaluatePiece(true,true,true,true,9)', 1);
+addPuzzlePiece('buttons', false, true, true, true, 'evaluatePiece(false,true,true,true,8)', 2);
+addPuzzlePiece('buttons', true, false, true, true, 'evaluatePiece(true,false,true,true,7)', 1);
+addPuzzlePiece('buttons', true, true, false, true, 'evaluatePiece(true,true,false,true,6)', 2);
+addPuzzlePiece('buttons', true, true, true, false, 'evaluatePiece(true,true,true,false,5)', 1);
+addPuzzlePiece('buttons', false, true, true, false, 'evaluatePiece(false,true,true,false,4)', 2);
+addPuzzlePiece('buttons', false, false, true, true, 'evaluatePiece(false,false,true,true,3)', 1);
+addPuzzlePiece('buttons', true, true, false, false, 'evaluatePiece(true,true,false,false,2)', 2);
+addPuzzlePiece('buttons', true, false, false, true, 'evaluatePiece(true,false,false,true,1)', 1);
 
 function addPuzzlePiece(toId, hasTop, hasRight, hasBottom, hasLeft, onclick, color, leftPosition, topPosition) {
    let style = '';
@@ -30,39 +30,68 @@ function addPuzzlePiece(toId, hasTop, hasRight, hasBottom, hasLeft, onclick, col
          ${hasLeft ? `<span class="l"></span>` : ''}
       </div> 
       `;
+}
+
+function addPuzzlePieceToBoard(hasTop, hasRight, hasBottom, hasLeft) {
+   addPuzzlePiece('board', hasTop, hasRight, hasBottom, hasLeft, '', jigsawColor, verticalPosition, horizontalPosition);
+   verticalPosition -= 6;
+   timesAdded++;
+   colorSwitch();
+   checkTimesAdded();
+}
+
+function checkTimesAdded() {
+   if (timesAdded == 5) {
+      verticalPosition = 24;
+      horizontalPosition += 6;
+      timesAdded = 0;
    }
+}
 
-   function addPuzzlePieceToBoard(hasTop, hasRight, hasBottom, hasLeft) {
-      addPuzzlePiece('board', hasTop, hasRight, hasBottom, hasLeft, '', jigsawColor, verticalPosition, horizontalPosition);
-      verticalPosition -= 6;
-      timesAdded++;
-      colorSwitch();
-      checkTimesAdded();
+function colorSwitch() {
+   if (jigsawColor == 1) {
+      jigsawColor = 2;
+   } else {
+      jigsawColor = 1;
    }
+}
 
-   function checkTimesAdded() {
-      if (timesAdded == 5) {
-         verticalPosition = 24;
-         horizontalPosition += 6;
-         timesAdded = 0;
-      }
+function evaluatePiece(hasTop, hasRight, hasBottom, hasLeft,pieceId) {
+   if (horizontalPosition == 0) {
+      evaluateFirstRow(hasTop, hasRight, hasBottom, hasLeft,pieceId)
+   } else if (horizontalPosition < 24 && horizontalPosition > 0) {
+      evaluateMiddleRows(hasTop, hasRight, hasBottom, hasLeft,pieceId)
+   } else if (horizontalPosition == 24) {
+      evaluateBottomRow(hasTop, hasRight, hasBottom, hasLeft,pieceId)
    }
+}
 
-   function colorSwitch() {
-      if (jigsawColor == 1) {
-         jigsawColor = 2;
-      } else {
-         jigsawColor = 1;
-      }
+function evaluateFirstRow(hasTop, hasRight, hasBottom, hasLeft,pieceId) {
+   if (verticalPosition == 24 && pieceId == 3) {
+      addPuzzlePieceToBoard(hasTop, hasRight, hasBottom, hasLeft);
+   } else if (verticalPosition < 24 && verticalPosition > 0 && pieceId == 8) {
+      addPuzzlePieceToBoard(hasTop, hasRight, hasBottom, hasLeft);
+   } else if (verticalPosition == 0 && pieceId == 4) {
+      addPuzzlePieceToBoard(hasTop, hasRight, hasBottom, hasLeft)
    }
+}
 
-   function evaluatePiece(hasTop,hasRight,hasBottom,hasLeft) {
-      if (horizontalPosition == 0 && verticalPosition == 24 && !hasTop && !hasRight) {
-         addPuzzlePieceToBoard(hasTop, hasRight, hasBottom, hasLeft)
-      }  else if (horizontalPosition > 24 && verticalPosition == 0 && !hasTop) {
-         addPuzzlePieceToBoard(hasTop, hasRight, hasBottom, hasLeft) 
-      }
+function evaluateMiddleRows(hasTop, hasRight, hasBottom, hasLeft,pieceId) {
+   if (verticalPosition == 24 && pieceId == 7) {
+      addPuzzlePieceToBoard(hasTop, hasRight, hasBottom, hasLeft);
+   } else if (verticalPosition < 24 && verticalPosition > 0 && pieceId == 9) {
+      addPuzzlePieceToBoard(hasTop, hasRight, hasBottom, hasLeft);
+   } else if (verticalPosition == 0 && pieceId == 5) {
+      addPuzzlePieceToBoard(hasTop, hasRight, hasBottom, hasLeft)
    }
+}
 
-
-   
+function evaluateBottomRow(hasTop, hasRight, hasBottom, hasLeft,pieceId) {
+   if (verticalPosition == 24 && pieceId == 1) {
+      addPuzzlePieceToBoard(hasTop, hasRight, hasBottom, hasLeft);
+   } else if (verticalPosition < 24 && verticalPosition > 0 && pieceId == 6) {
+      addPuzzlePieceToBoard(hasTop, hasRight, hasBottom, hasLeft);
+   } else if (verticalPosition == 0 && pieceId == 2) {
+      addPuzzlePieceToBoard(hasTop, hasRight, hasBottom, hasLeft)
+   }
+}
